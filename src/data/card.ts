6602b0d84c8bd49145d8ffacbdce1092e9f22286
macmeals <loseworld9731@gmail.com>
2022-07-08 23:51:03 +0900

    firstCommit

---
+++ src/data/card.ts
@@ -0,0 +1,27 @@
+export const cardData = [
+  {
+    id: "１",
+    title: "庭の水やり",
+    todo: "実家の水やりをすること",
+  },
+  {
+    id: "２",
+    title: "Reactの学習",
+    todo: "なんでもメモアプリを作成",
+  },
+  {
+    id: "３",
+    title: "TypeScriptの実装",
+    todo: "TypeScriptの型付けをコンポーネントで実装",
+  },
+  {
+    id: "4",
+    title: "デザインカンプを作成",
+    todo: "デザインコンテストを実施して応募",
+  },
+  {
+    id: "5",
+    title: "イラスト",
+    todo: "イラストの練習（線画や色塗り）を毎日継続する",
+  },
+];
