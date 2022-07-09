5276fa349fa6cc8a08134119bc653382f0f35db6
macmeals <loseworld9731@gmail.com>
2022-07-09 17:05:51 +0900

    styledComponent react-beautiful-dnd 導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
-import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { cardData } from "./data/card";
 
-function App() {
+// import React from "react";
+import styled from "styled-components";
+
+const Scontainer = styled.div`
+  border: solid 1px #aaa;
+  border-radius: 20px;
+  padding: 8px;
+  margin: 8px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 300px;
+`;
+
+const TodoCards = styled.div`
+  margin-top: 30px;
+  border: solid 1px #aaa;
+  width: 400px;
+`;
+
+export const App = () => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <TodoCards>
+        <ul>
+          {cardData.map((data) => {
+            return (
+              <Scontainer key={data.id}>
+                <p>{data.title}:</p>
+                <p>{data.todo}</p>
+              </Scontainer>
+            );
+          })}
+        </ul>
+      </TodoCards>
     </div>
   );
-}
+};
 
 export default App;
